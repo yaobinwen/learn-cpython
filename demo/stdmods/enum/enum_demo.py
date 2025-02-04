419298f6fe8775e7c01699cbfae4f8c82ee1ca2b
Yaobin Wen <robin.wyb@gmail.com>
2025-02-04 17:26:28 -0500

    Update `enum` demo code

--- demo/stdmods/enum/enum_demo.py
+++ demo/stdmods/enum/enum_demo.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-
+# coding: UTF-8
 
 """This file contains examples of using `enum`. For its documentation, see
 https://docs.python.org/3/library/enum.html
@@ -39,6 +38,27 @@ class TestEnum(unittest.TestCase):
         ):
             self.assertEqual(e.name, name_value[0])
             self.assertEqual(e.value, name_value[1])
+
+
+NAME_ALICE = "Alice"
+NAME_BOB = "Bob"
+
+
+class Name(enum.StrEnum):
+    ALICE = NAME_ALICE
+    BOB = NAME_BOB
+
+    @staticmethod
+    def has_value(s):
+        return s == Name.ALICE or s == Name.BOB
+
+
+class TestEnum(unittest.TestCase):
+    def test_has_value(self):
+        self.assertTrue(Name.has_value(NAME_ALICE))
+        self.assertTrue(Name.has_value(Name.ALICE))
+
+        self.assertFalse(Name.has_value("alicE"))
 
 
 if __name__ == "__main__":
